9fc8cfc4fafb8447c74d7bceb8add1d79fa75b1f
vella <vadmir04@gmail.com>
2023-09-03 23:33:23 +0200

    Add token model, type and repository.

---
+++ unoLikeGame/src/main/java/com/vella/unoLikeGame/token/TokenRepository.java
@@ -0,0 +1,18 @@
+package com.vella.security.token;
+
+import java.util.List;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+public interface TokenRepository extends JpaRepository<Token, Integer> {
+
+  @Query(value = """
+      SELECT t FROM Token t INNER JOIN User u\s
+      ON t.user.id = u.id\s
+      WHERE u.id = :id AND (t.expired = FALSE OR t.revoked = FALSE)\s
+      """)
+  List<Token> findAllValidTokenByUser(Integer id);
+
+  Optional<Token> findByToken(String token);
+}
