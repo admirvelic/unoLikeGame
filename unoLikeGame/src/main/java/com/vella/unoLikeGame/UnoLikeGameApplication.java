9388f586682b5b599f718b349eef41854a38d86f
vella <vadmir04@gmail.com>
2023-09-03 23:26:10 +0200

    Initial commit

---
+++ unoLikeGame/src/main/java/com/vella/unoLikeGame/UnoLikeGameApplication.java
@@ -0,0 +1,13 @@
+package com.vella.unoLikeGame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnoLikeGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnoLikeGameApplication.class, args);
+	}
+
+}
---
+++ unoLikeGame/src/test/java/com/vella/unoLikeGame/UnoLikeGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vella.unoLikeGame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnoLikeGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
